9b9bad064362a7926aec62635e68bf1442ed2a79
luiscaputo <luiscaputo15@gmail.com>
2023-09-13 21:08:19 +0100

    feat: Queues

---
+++ queue.lua
@@ -0,0 +1,9 @@
+local queue = {
+  group_1 = { "ViperGT" },
+  group_4 = { "BlazeGamer", "SpeedRacer", "ShadowNinja", "PhoenixFire" },
+  group_6 = { "ThunderBolt", "GhostRider" },
+  group_9 = { "NeonSpectre", "DriftKing" },
+  group_10 = { "MidnightWolf" }
+}
+
+return queue
